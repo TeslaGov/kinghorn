dc64e7ead970d64c6280f1c3832028a40ddd8026
Jon Crain <jon.crain@teslagovernment.com>
2019-05-28 15:48:33 -0400

    Fix namespacing

--- kinghorn/__init__.py
+++ kinghorn/__init__.py
@@ -1,4 +1,14 @@
-import kinghorn
+from kinghorn.kinghorn import cache_all_if_needed
+from kinghorn.kinghorn import cache_snapshot_info
+from kinghorn.kinghorn import cache_volume_info
+from kinghorn.kinghorn import cache_all_ec2
+from kinghorn.kinghorn import cache_instance_info
+from kinghorn.kinghorn import clean_cache
+from kinghorn.kinghorn import load_info_from_cache
+from kinghorn.kinghorn import get_instance_id_to_name_map
+from kinghorn.kinghorn import get_instance_id_to_volume_id_map
+from kinghorn.kinghorn import get_instance_name_to_id_map
+from kinghorn.kinghorn import get_volume_id_to_instance_id_map
 
 __author__ = "Jon Crain"
-__version__ = "0.0.3"
+__version__ = "0.0.6"
